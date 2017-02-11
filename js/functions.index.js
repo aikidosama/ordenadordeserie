48d06c4af5bacd5daca5e296a383df190be9689e
aikidosama <aikidosama@yahoo.com>
2017-02-11 16:58:50 -0500

    Update functions.index.js

--- js/functions.index.js
+++ js/functions.index.js
@@ -4,12 +4,12 @@ $(document).ready(function () {
     var buttonAddNew = document.querySelector('[data-ref="button-add-new"]');
     var i = $('#integ div').size() + 1;
 
-    var mixer = mixitup(containerEl, {
+    var mixer = mixitup(containerEl, { //inicializa una variable de mixitup que contiene al objeto y sus selectores 
         selectors: {
             target: '[data-ref="mix1"]'
         }
     });
     buttonAddNew.addEventListener('click', function() {
-        mixer.insertAfter('<div class="mix1" data-ref="mix1" data-myorder="' + i +'"><input type="text" data-validation="number" data-validation-allowing="range[-99;999],negative" step="1" value="" maxlength="3" id="integ" name="integ[]"  value="" placeholder="número" /></div>', buttonAddNew);
+        mixer.insertAfter('<div class="mix1" data-ref="mix1" data-myorder="' + i +'"><input type="text" data-validation="number" data-validation-allowing="range[-99;999],negative" step="1" value="" maxlength="3" id="integ" name="integ[]"  value="" placeholder="número" /></div>', buttonAddNew);  // Función de mixitup que agrega un elemento adicional al grupo numérico
     });
 });
